296748ebb30f5c557c68851384ef642a87658823
lcohen730 <lcohen730@gmail.com>
2024-07-23 23:27:25 -0400

    Add Functionality to Arrows for Carousel Images

--- script.js
+++ script.js
@@ -1,27 +1,41 @@
 /*----- constants -----*/
 
-const images = [];
+const carouselImages = [
+    "./design/images/desktop-image-hero-1.jpg", 
+    "./design/images/desktop-image-hero-2.jpg", 
+    "./design/images/desktop-image-hero-3.jpg"
+];
 
 /*----- state variables -----*/
 
-let currentImage = 0;
+let currentSlide = 0;
 
 /*----- cached elements  -----*/
 
-const leftArrow = documment.getElementById('left-arrow');
-const rightArrow = documment.getElementById('right-arrow');
+const leftArrow = document.getElementById('left-arrow');
+const rightArrow = document.getElementById('right-arrow');
+const carouselImage = document.getElementById('img1');
 
 /*----- event listeners -----*/
 
-leftArrow.addEventListener('click', carouselLeft)
-rightArrow.addEventListener('click', carouselRight)
+leftArrow.addEventListener('click', () => carouselShift('left'));
+rightArrow.addEventListener('click', () => carouselShift('right'));
 
 /*----- functions -----*/
 
-function carouselLeft() {
-    console.log('left');
-}
-
-function carouselRight() {
-    console.log('right');
+function carouselShift(direction) {
+    if (direction === 'right') {
+        if (currentSlide < carouselImages.length - 1) {
+            currentSlide++;
+        } else {
+            currentSlide = 0;
+        }
+    } else if (direction === 'left') {
+        if (currentSlide > 0) {
+            currentSlide--;
+        } else {
+            currentSlide = carouselImages.length - 1;
+        }
+    }
+    carouselImage.style.backgroundImage = `url(${carouselImages[currentSlide]})`;
 }
